019ff6cd8570ba83e964c0c102d940908b8d76b9
chenqian <chenqian@parim.net>
2019-01-24 18:10:55 +0800

    commit back code

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+const router = require('koa-router')()
+
+router.get('/', async (ctx, next) => {
+  await ctx.render('index', {
+    title: 'Hello Koa 2!'
+  })
+})
+
+router.get('/string/:id', async (ctx, next) => {
+  console.log(ctx.queryString);
+  ctx.body = 'koa2 string'
+})
+
+router.get('/json', async (ctx, next) => {
+  ctx.body = {
+    title: 'koa2 json'
+  }
+})
+
+module.exports = router
